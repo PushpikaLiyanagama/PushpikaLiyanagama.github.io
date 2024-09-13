5e4db892c5eaedba726d70926c87ed1f4f03aa31
PushpikaLiyanagama <21ug0889@sltc.ac.lk>
2024-09-13 11:28:51 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<PushpikaLiyanagama.github.io>/'
+  base: '/PushpikaLiyanagama.github.io/'
 });
